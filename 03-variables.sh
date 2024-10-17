1bd2515eb5d4174fc39c12a56bb1f9c9047ace72
praveensri453 <praveensri92@gmail.com>
2024-10-17 11:28:39 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "${PERSON1}:: hi ${PERSON2} , how r u?"
+echo "${PERSON2}:: hi ${PERSON1}, i am good ,h r u?"
+echo "${PERSON1}:: i am doing good , whats going on?"
+echo "${PERSON2}:: started  learning devops ${PERSON1}"
