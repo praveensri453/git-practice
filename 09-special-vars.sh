45163a2e1beac3661a9573bd19e19d0b695fd9b4
praveensri453 <praveensri92@gmail.com>
2024-10-19 16:37:10 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "all variables passed to the script: $@"
+echo "Number of variables passed to the script: $#"
+echo "script name: $0"
+echo "current working directory:$PWD"
+echo "home directory of current user:$HOME"
+echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
