1829641c855fb6363f17fa9034bd1c1d3c374d6c
praveensri453 <praveensri92@gmail.com>
2024-10-21 14:25:32 +0530

    loops

--- 17-set-ex1.sh
+++ 17-set-ex1.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+set -e 
 echo "Hello World success"
 echooooo "Hello Wolrd failure"
 echo "Hello World after failure"
+
+#op
+
+
+
+
+
+
+#ip
+
+#echo "Hello World success"
+#echooooo "Hello Wolrd failure"
+#echo "Hello World after failure"
+
+#output
+
+#Hello World success
+#line 3: echooooo: command not found
+#Hello World after failure
