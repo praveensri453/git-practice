6719e0b56f99e3d87fe831866fab88e6bebe268b
praveensri453 <praveensri92@gmail.com>
2024-10-21 15:48:50 +0530

    loops

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -14,3 +14,10 @@ else
 fi
 FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 echo "Files: $FILES"
+
+# dont use line, it is reserverd word
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+    echo "Deleting file: $file"
+    rm -rf $file
+done <<< $FILES
