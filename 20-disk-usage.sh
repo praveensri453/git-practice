8fe819765980a1ed3792365335d2fa9dba72e652
praveensri453 <praveensri92@gmail.com>
2024-10-22 14:40:23 +0530

    loops

---
+++ 20-disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SOURCE=${df -hT|grep xfs}
+THRESHOULD=30
+
+
+while IFS=read -r file
+do
+    CU_VAL=${$SOURCE|awk -F " " '{print $6}'|cut -d "%" -f6}
+    PATH=$SOURCE|awk -F " " '{print $NF}'
+    if [ $CU_VAL -gt $THRESHOULD ]
+    
+    then echo "$CU_VAL is greater than $THRESHOULD"
+    fi
+
+done <<< $SOURCE    
