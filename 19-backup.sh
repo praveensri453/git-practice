b8c3523a4223bcecf9a96d457c552129a55372d7
praveensri453 <praveensri92@gmail.com>
2024-10-22 10:24:45 +0530

    loops

--- 19-backup.sh
+++ 19-backup.sh
@@ -40,6 +40,21 @@ then
     echo "Files are found"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
-    echo "no files are older than : $DAYS"
-fi        
+    echo "No files older than $DAYS"
+fi
