398afcb1191ac43839e32652df6ad9f4d01cbbc7
praveensri453 <praveensri92@gmail.com>
2024-10-22 10:28:35 +0530

    loops

--- 19-backup.sh
+++ 19-backup.sh
@@ -35,7 +35,7 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+if [ ! -z $FILES ] #true if FILES is empty
 then
     echo "Files are found"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
